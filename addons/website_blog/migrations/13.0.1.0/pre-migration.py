d9a7a8d7db432f3fd511109530902b3364bb42a2
Antonio Ruban <antoniodavid8@gmail.com>
2022-06-02 10:40:10 -0400

    [FIX][website_blog] Comment renames xmlid

--- addons/website_blog/migrations/13.0.1.0/pre-migration.py
+++ addons/website_blog/migrations/13.0.1.0/pre-migration.py
@@ -22,5 +22,5 @@ def switch_noupdate_records(env):
 def migrate(env, version):
     openupgrade.load_data(
         env.cr, "website_blog", "migrations/13.0.1.0/noupdate_changes.xml")
-    openupgrade.rename_xmlids(env.cr, _xmlid_renames)
+    # openupgrade.rename_xmlids(env.cr, _xmlid_renames)
     switch_noupdate_records(env)
