a7b6ccd1df13dd4aba94f2d8968f45bf7de3f501
Antonio Ruban <antoniodavid8@gmail.com>
2022-06-02 10:02:54 -0400

    [ADD] Migration v13

--- odoo/addons/base/migrations/13.0.1.3/pre-migration.py
+++ odoo/addons/base/migrations/13.0.1.3/pre-migration.py
@@ -784,6 +784,44 @@ def rename_ir_module_category(env):
                 openupgrade.rename_xmlids(env.cr, [(old_xmlid, new_xmlid)])
 
 
+def fix_double_membership(cr):
+    # avoid error raised by new function '_check_one_user_type'
+
+    # assuming that group_public < group_portal < group_user
+    # this script keept the highest group, if a user belong to many
+    # groups
+    confs = [
+        ("group_public", "group_portal"),
+        ("group_public", "group_user"),
+        ("group_portal", "group_user"),
+    ]
+    for conf in confs:
+        group_to_remove = conf[0]
+        group_to_keep = conf[1]
+        openupgrade.logged_query(
+            cr, """
+                DELETE FROM res_groups_users_rel
+                WHERE
+                gid = (
+                    SELECT res_id
+                    FROM ir_model_data
+                    WHERE module = 'base' AND name = %s
+                )
+                AND uid IN (
+                    SELECT uid FROM res_groups_users_rel WHERE gid IN (
+                        SELECT res_id
+                        FROM ir_model_data
+                        WHERE module = 'base'
+                        AND name IN (%s, %s)
+                    )
+                    GROUP BY uid
+                    HAVING count(*) > 1
+                );
+            """, (group_to_remove, group_to_remove, group_to_keep)
+        )
+
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.remove_tables_fks(env.cr, _obsolete_tables)
@@ -822,3 +860,5 @@ def migrate(env, version):
         """ UPDATE ir_model_constraint
         SET write_date = date_update
         WHERE write_date IS NULL AND date_update IS NOT NULL """)
+
+    fix_double_membership(env.cr)
