ba5d7f821557d32530d4656deef84378f6d5d629
Antonio Ruban <antoniodavid8@gmail.com>
2022-06-11 11:31:17 -0400

    [FIX] Duplicidad de groups en hr

--- addons/hr/migrations/12.0.1.1/pre-migration.py
+++ addons/hr/migrations/12.0.1.1/pre-migration.py
@@ -17,20 +17,45 @@ xmlid_renames = [
 
 def fix_users_belong_two_groups(env):
     _logger.info('Fixing users with conflicting groups')
-    users = env['res.users'].search([], order='id asc')
-    for user in users:
-        if user.has_group('base.group_user') and (
-                user.has_group('base.group_private_addresses')
-        ):
-            _logger.warning('Conflict in user groups: %s' % user.name)
-            user.write({'groups_id': [
-                (3, env.ref('base.group_private_addresses').id),
-            ]})
+    # avoid error raised by new function '_check_one_user_type'
+
+    # assuming that group_public < group_portal < group_user
+    # this script keept the highest group, if a user belong to many
+    # groups
+    confs = [
+        ("group_public", "group_portal"),
+        ("group_public", "group_user"),
+        ("group_portal", "group_user"),
+    ]
+    for conf in confs:
+        group_to_remove = conf[0]
+        group_to_keep = conf[1]
+        openupgrade.logged_query(
+            env.cr, """
+                    DELETE FROM res_groups_users_rel
+                    WHERE
+                    gid = (
+                        SELECT res_id
+                        FROM ir_model_data
+                        WHERE module = 'base' AND name = %s
+                    )
+                    AND uid IN (
+                        SELECT uid FROM res_groups_users_rel WHERE gid IN (
+                            SELECT res_id
+                            FROM ir_model_data
+                            WHERE module = 'base'
+                            AND name IN (%s, %s)
+                        )
+                        GROUP BY uid
+                        HAVING count(*) > 1
+                    );
+                """, (group_to_remove, group_to_remove, group_to_keep)
+        )
+
 
 @openupgrade.migrate()
 def migrate(env, version):
-    fix_users_belong_two_groups(env)
     if openupgrade.column_exists(env.cr, 'hr_employee', 'vehicle_distance'):
         openupgrade.rename_fields(env, _field_renames)
     openupgrade.rename_xmlids(env.cr, xmlid_renames)
-    
+    fix_users_belong_two_groups(env)
